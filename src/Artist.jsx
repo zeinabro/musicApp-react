7a2e40a446f13d00cbd0284d92b458b490a23674
Samin Chowdhury <samin1001@gmail.com>
2023-07-10 15:39:22 +0100

    added Artist.jsx

---
+++ src/Artist.jsx
@@ -0,0 +1,32 @@
+import React from "react"
+import {createRoot} from "react-dom/client"
+
+const data = [
+    { artistName: "Queen", genre: "Rock", description: "We are Queen ..." },
+]
+
+export default function Artist(){
+   
+    return (
+        <>
+        <div className="Artist">
+            <table>
+                <tr>
+                    <th>Artist</th>
+                    <th>Genre</th>
+                    <th>Description</th>
+                </tr>
+                {data.map((val, key) => {
+                    return (
+                        <tr key={key}>
+                            <td>{val.artistName}</td>
+                            <td>{val.genre}</td>
+                            <td>{val.description}</td>
+                        </tr>
+                    )
+                })}
+            </table>
+        </div>
+        </>    
+    )
+}
---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+import {createRoot} from "react-dom/client"
+import Artist from "./Artist"
+
+const root = createRoot(document.getElementById("root"))
+root.render(<>
+    <Artist/>
+   
+</>)
